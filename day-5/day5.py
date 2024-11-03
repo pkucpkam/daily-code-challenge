9d5cf65f8229c180f198fde5a7719008189a1959
Pham Van Phuc <phucpham.1803@gmail.com>
2024-11-03 18:08:16 +0700

    day 5

---
+++ day-5/day5.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s or len(s) < 1:
+            return ""
+        
+        start, end = 0, 0
+        
+        for i in range(len(s)):
+            left, right = i, i
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            len1 = right - left - 1  
+            
+            left, right = i, i + 1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            len2 = right - left - 1  
+            
+            max_len = max(len1, len2)
+            
+            if max_len > end - start:
+                start = i - (max_len - 1) // 2
+                end = i + max_len // 2
+        
+        return s[start:end + 1]
