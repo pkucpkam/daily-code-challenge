e9878636ccddad38169a46fd4d87aa700342ad18
Pham Van Phuc <phucpham.1803@gmail.com>
2025-07-07 23:51:45 +0700

    day 3 ...

---
+++ problem-31/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder filtered = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                filtered.append(Character.toLowerCase(c));
+            }
+        }
+        
+        String str = filtered.toString();
+        int left = 0, right = str.length() - 1;
+        
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
