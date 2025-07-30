ce1c8d50896226ea11a47ba2c48ae352d5c40488
Pham Van Phuc <phucpham.1803@gmail.com>
2025-07-30 23:34:07 +0700

    day 20

---
+++ problem-78/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] charCount = new int[26];
+        for (char c : s.toCharArray()) {
+            charCount[c - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (charCount[s.charAt(i) - 'a'] == 1)
+                return i;
+        }
+        return -1;
+    }
+}
