65e961ef60eefca9a5d36c97c06ddc2466fcc8de
Pham Van Phuc <phucpham.1803@gmail.com>
2025-08-24 23:38:37 +0700

    day 44

---
+++ problems-from-101-to-200/problem-120/Solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        backtrack(res, new StringBuilder(), 0, 0, n);
+        return res;
+    }
+
+    private void backtrack(List<String> res, StringBuilder sb, int open, int close, int max) {
+        if (sb.length() == max * 2) {
+            res.add(sb.toString());
+            return;
+        }
+        if (open < max) {
+            sb.append('(');
+            backtrack(res, sb, open + 1, close, max);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        if (close < open) {
+            sb.append(')');
+            backtrack(res, sb, open, close + 1, max);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
