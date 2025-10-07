2580bf50573643675ff7763a89c6f87cd5abfb47
Pham Van Phuc <phucpham.1803@gmail.com>
2025-10-07 21:42:33 +0700

    day 88

---
+++ problems-from-101-to-200/problem-176/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> grayCode(int n) {
+        int size = 1 << n;
+        List<Integer> res = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            res.add(i ^ (i >> 1)); 
+        }
+        return res;
+    }
+}
