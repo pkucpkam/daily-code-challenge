e502ab2094a0afa9365dae8ab909d08b5c561c11
Pham Van Phuc <phucpham.1803@gmail.com>
2025-10-08 23:45:35 +0700

    day 89

---
+++ problems-from-101-to-200/problem-177/Solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(nums, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void backtrack(int[] nums, int start, List<Integer> curr, List<List<Integer>> res) {
+        res.add(new ArrayList<>(curr));
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue; // skip duplicates at same recursion level
+            curr.add(nums[i]);
+            backtrack(nums, i + 1, curr, res);
+            curr.remove(curr.size() - 1);
+        }
+    }
+}
