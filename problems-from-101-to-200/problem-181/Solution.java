0e4f482af8b0a861f5c48689b7aa7ee4eb7c259f
Pham Van Phuc <phucpham.1803@gmail.com>
2025-10-12 23:26:49 +0700

    day 93

---
+++ problems-from-101-to-200/problem-181/Solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || left == right) return head;
+
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
+
+        // move prev to the node immediately before position left
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
+        }
+
+        // curr points to the first node of the sublist to reverse
+        ListNode curr = prev.next;
+
+        // reverse the sublist by head-insertion method
+        for (int i = 0; i < right - left; i++) {
+            ListNode next = curr.next;
+            curr.next = next.next;
+            next.next = prev.next;
+            prev.next = next;
+        }
+
+        return dummy.next;
+    }
+}
