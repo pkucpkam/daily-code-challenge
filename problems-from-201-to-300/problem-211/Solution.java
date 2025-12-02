93140ae5b770af13c4d9d3fb46935cd0aa8de5ea
Pham Van Phuc <phucpham.1803@gmail.com>
2025-12-03 05:33:21 +0700

    day 122

---
+++ problems-from-201-to-300/problem-211/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+        long total = 0;
+        long tank = 0;
+        int start = 0;
+        for (int i = 0; i < n; i++) {
+            int diff = gas[i] - cost[i];
+            total += diff;
+            tank += diff;
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        return total >= 0 ? start % n : -1;
+    }
+}
